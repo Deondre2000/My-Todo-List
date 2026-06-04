4a5df9c3e0b75fb23abd03c91b91fb565837f845
deondre butler <deondrebutler20@gmail.com>
2026-06-04 12:30:26 -0400

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="Footer">
       <p className="Footer__text">
-        © 2024 My To-Do List. All rights reserved by Deondre Butler
+        © 2024 To-Do List. All rights reserved by Deondre Butler
       </p>
     </footer>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import addIcon from "../assets/add.png";
 function Header({ onAddClick }) {
   return (
     <header className="header">
-      <h1 className="Title">To Do List</h1>
+      <h1 className="Title">To-Do List</h1>
       <button type="button" className="Headeradd__Todo" onClick={onAddClick}>
         <img className="Headeradd__Icon" src={addIcon} alt="Add Todo" />
         Add Todo

a17516c63ca34cbdf5079b0f0a7552cdd38146a3
deondre butler <deondrebutler20@gmail.com>
2026-07-16 17:34:30 -0400

    finish adding routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import "../blocks/App.css";
 import Header from "./Header.jsx";
 import Content from "./Content.jsx";
@@ -112,16 +113,21 @@ function App() {
     <div className="App">
       <img className="App__background" src={background} alt="Background" />
       <Nav />
-      <Header onAddClick={openModal} />
-      {isLoading && <p>Loading todos...</p>}
-      {!isLoading && error && <p>{error}</p>}
-      <Content
-        todos={todos}
-        onDeleteTodo={handleDeleteTodo}
-        onToggleTodo={toggleTodo}
-      />
-      {isModalOpen && <Modal onClose={closeModal} onAddTodo={addTodo} />}
-      <Footer />
+
+      <Routes>
+        <Route>
+          <Header onAddClick={openModal} />
+          {isLoading && <p>Loading todos...</p>}
+          {!isLoading && error && <p>{error}</p>}
+          <Content
+            todos={todos}
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={toggleTodo}
+          />
+          {isModalOpen && <Modal onClose={closeModal} onAddTodo={addTodo} />}
+          <Footer />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/timer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import image from "../assets/reborn.webp";
+
+export default function TimerPage() {
+  return (
+    <div>
+      <h1>Focus Timer</h1>
+      <img src={image} alt="Focus timer" />
+    </div>
+  );
+}
